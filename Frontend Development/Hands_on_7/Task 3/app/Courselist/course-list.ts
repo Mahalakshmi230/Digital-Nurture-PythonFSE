790e3409a6a507a3b219e883bed4b6cb866ea104
Mahalakshmi230 <149365324+Mahalakshmi230@users.noreply.github.com>
2026-07-14 13:51:57 +0530

    Add files via upload

---
+++ Frontend Development/Hands_on_7/Task 3/app/Courselist/course-list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-course-list',
+  imports: [],
+  templateUrl: './course-list.html',
+  styleUrl: './course-list.css'
+})
+export class CourseListComponent {
+
+
+}
