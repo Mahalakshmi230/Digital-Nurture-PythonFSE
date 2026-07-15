dafd93297c8e73ecf312ae1f04680b40afceacf9
Mahalakshmi230 <149365324+Mahalakshmi230@users.noreply.github.com>
2026-07-15 09:46:33 +0530

    Add files via upload

---
+++ Frontend Development/Hands_on_8/Task 3/Components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+
+    <h1>Student Portal</h1>
+
+    <h3>
+      Enrolled Courses :
+      {{ store.enrolledCourses.length }}
+    </h3>
+
+  </header>
+</template>
+
+<script setup>
+import { useEnrollmentStore } from '../stores/enrollment'
+
+const store = useEnrollmentStore()
+</script>
+
+<style scoped>
+.header {
+  background: #42b883;
+  color: white;
+  text-align: center;
+  padding: 18px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+</style>
