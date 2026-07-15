d47c7033c6107f3e02cb24adb697b78fc2efb095
Mahalakshmi230 <149365324+Mahalakshmi230@users.noreply.github.com>
2026-07-15 08:47:10 +0530

    Add files via upload

---
+++ Frontend Development/Hands_on_8/Task 1/Components/CourseCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <h3>{{ name }}</h3>
+
+    <p><b>Code:</b> {{ code }}</p>
+
+    <p><b>Credits:</b> {{ credits }}</p>
+
+    <p><b>Grade:</b> {{ grade }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  code: String,
+  credits: Number,
+  grade: String
+})
+</script>
+
+<style scoped>
+.card {
+  width: 180px;
+  background: white;
+  border-radius: 10px;
+  padding: 15px;
+  box-shadow: 0px 2px 8px rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+
+.card:hover {
+  transform: scale(1.05);
+}
+
+h3 {
+  color: #42b883;
+  margin-bottom: 10px;
+}
+
+p {
+  margin: 5px 0;
+}
+</style>
---
+++ Frontend Development/Hands_on_8/Task 1/Components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <h1>Student Portal</h1>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header {
+  background-color: #42b883;
+  color: white;
+  padding: 15px;
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
