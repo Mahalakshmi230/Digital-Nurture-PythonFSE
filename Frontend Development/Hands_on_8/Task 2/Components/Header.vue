5817edb5961e6e6131490432684121f50d78efca
Mahalakshmi230 <149365324+Mahalakshmi230@users.noreply.github.com>
2026-07-15 09:16:19 +0530

    Add files via upload

---
+++ Frontend Development/Hands_on_8/Task 2/Components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <h1>Student Portal</h1>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header {
+  background: #42b883;
+  color: white;
+  text-align: center;
+  padding: 18px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+</style>
