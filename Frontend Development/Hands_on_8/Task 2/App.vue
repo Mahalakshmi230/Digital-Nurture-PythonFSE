374e5337694a37cb6e5ea8228e34b3cf27ec6732
Mahalakshmi230 <149365324+Mahalakshmi230@users.noreply.github.com>
2026-07-15 09:19:36 +0530

    Add files via upload

---
+++ Frontend Development/Hands_on_8/Task 2/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+
+    <Header />
+
+    <nav>
+
+      <RouterLink to="/">Home</RouterLink>
+
+      <RouterLink to="/courses">Courses</RouterLink>
+
+      <RouterLink to="/profile">Profile</RouterLink>
+
+    </nav>
+
+    <RouterView />
+
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  background: #f4f4f4;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  width: 700px;
+  margin-top: 20px;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  background: white;
+  padding: 15px;
+  margin-bottom: 20px;
+  border-radius: 8px;
+}
+
+nav a {
+  text-decoration: none;
+  color: #42b883;
+  font-weight: bold;
+}
+
+.router-link-active {
+  color: #2c3e50;
+}
+</style>
